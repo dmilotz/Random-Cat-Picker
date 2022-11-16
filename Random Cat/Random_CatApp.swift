5163409f46d5e9f615bf8e02b4999668997e1890
Dirk Milotz <dmilotz@gmail.com>
2022-11-16 11:34:06 -0800

    Initial Commit

---
+++ Random Cat/Random_CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_CatApp.swift
+//  Random Cat
+//
+//  Created by Dirk Milotz on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Random_CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
